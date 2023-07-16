17091398e55298f8ceacbdda831277849c061013
jacqui-maclean <inaripaddler@gmail.com>
2023-07-16 16:17:51 +0100

    clear page

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,6 @@ import { GridContainer } from "@/components/grid-container";
 export default function HomePage() {
   return (
     <GridContainer>
-      {/* <Title>Moot FE Tech Test</Title>
-
-      <IntroCopy />
-
-      <div className="flex h-full w-full flex-col">
-        <div className="my-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
-          <SuccessCriteria />
-          <div className="flex flex-col gap-4">
-            <UsefulLinks />
-            <CliCommands />
-          </div>
-        </div>
-      </div> */}
       <div>empty tab</div>
     </GridContainer>
   );
