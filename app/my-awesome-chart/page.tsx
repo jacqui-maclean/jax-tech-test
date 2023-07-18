17d0020209bb16c4ea29115818c72d281951782c
jacqui-maclean <inaripaddler@gmail.com>
2023-07-18 17:30:14 +0100

    improve chart title

--- app/my-awesome-chart/page.tsx
+++ app/my-awesome-chart/page.tsx
@@ -8,7 +8,7 @@ import MyChart from "./myChart";
 export default function AwesomeChart() {
   return (
     <GridContainer>
-      <Title>Exchange rates 2013/2023 GBP to USD</Title>
+      <Title>Daily exchange rates 2013/2023 GBP to USD</Title>
 
       <div className="flex h-full w-full flex-col">
         <MyChart />
