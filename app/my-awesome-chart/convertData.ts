f79f686f5e964b2e32c1e9bd66050660a5f799a7
jacqui-maclean <inaripaddler@gmail.com>
2023-07-18 16:49:23 +0100

    refactor converter to use reduce

--- app/my-awesome-chart/convertData.ts
+++ app/my-awesome-chart/convertData.ts
@@ -3,21 +3,19 @@ export interface ChartPoint {
   y: number;
 }
 
-const convertData = (rawData: { rates: { [key: string]: any } }) => {
-  let chartArray: { x: number; y: number }[] = [];
-  let rates = rawData.rates;
-  for (let key in rates) {
-    let chartObject = <ChartPoint>{};
-    let dateKey = rates[key];
-    let dateItems = key.split("-");
-    let day = dateItems[2];
-    chartObject.x = parseInt(day);
-    for (let key in dateKey) {
-      chartObject.y = dateKey[key];
-    }
-    chartArray.push(chartObject);
-  }
-  return chartArray;
-};
+interface Raw {
+  rates: { [key: string]: { USD: number } };
+}
+
+const convertData = (rawData: Raw) =>
+  Object.entries(rawData.rates).reduce<ChartPoint[]>((acc, curr) => {
+    let [date, currency] = curr;
+    let dateItems = date.split("-");
+    acc.push({
+      x: parseInt(dateItems[2]),
+      y: currency.USD,
+    });
+    return acc;
+  }, []);
 
 export default convertData;
